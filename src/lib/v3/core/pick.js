7af5b687771805f78a54ae96f9369476a389ecbd
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-06-12 18:55:39 +1000

    add docstring to oPick

--- src/lib/v3/core/pick.js
+++ src/lib/v3/core/pick.js
@@ -54,8 +54,14 @@ const assertType = type_name => {
   }
 }
 
+/**
+ * Return single arg from args after processing with mods. Returns null if arg name missing.
+ * @param {Object} args - All args. Obj shape is {arg: value, arg: value}
+ * @param {String} arg_name - Arg to return
+ * @param  {...Function} mods - Functions to modify target arg
+ */
 const oPick = (args, arg_name, ...mods) => {
-  const modArgs = mods.reduce((p, m) => ({ ...m(p, arg_name) }), args)
+  const modArgs = mods.reduce((p, mod) => ({ ...mod(p, arg_name) }), args)
   const result =
     (modArgs[arg_name] && { [arg_name]: modArgs[arg_name] }) || null
   return result
