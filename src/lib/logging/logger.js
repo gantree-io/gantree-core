b96c56598a43d83717b71230fda3670454a42f7c
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-06-23 17:23:44 +1000

    get logging levels from winston

--- src/lib/logging/logger.js
+++ src/lib/logging/logger.js
@@ -4,16 +4,7 @@ const { format } = winston
 const { combine, timestamp, label, printf, colorize } = format
 
 const getValidLevels = () => {
-  return {
-    emerg: 0,
-    alert: 1,
-    crit: 2,
-    error: 3,
-    warning: 4,
-    notice: 5,
-    info: 6,
-    debug: 7
-  }
+  return winston.config.syslog.levels
 }
 const getDefaultLevel = () => {
   return 'info'
