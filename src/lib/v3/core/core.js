e9dcfbf0f477b5e5dfaed6560eacfe485984cae2
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-06-29 16:07:22 +1000

    fix throw, handle no command, handle rejection

--- src/lib/v3/core/core.js
+++ src/lib/v3/core/core.js
@@ -1,6 +1,7 @@
 const {
   GantreeError,
-  ErrorTypes: { COMMAND_ERROR }
+  ErrorTypes: { UNSUPPORTED_OPERATION, MISSING_ARGUMENTS },
+  handleErr
 } = require('../../error/gantree-error')
 const Frame = require('./frame')
 const { sync } = require('./commands/sync')
@@ -14,12 +15,18 @@ const commandMap = {
 
 const run = async args => {
   const frame = Frame.createFrame(args)
+  if (!args.command) {
+    throw new GantreeError(
+      MISSING_ARGUMENTS, // TODO(Denver): Should be able to replace this and other similar instances with a generic func to check for required, then throw if not
+      "Must supply 'command'"
+    )
+  }
   const command = commandMap[args.command]
 
   if (!command) {
-    throw GantreeError(
-      COMMAND_ERROR,
-      `ConfigV2 - Unsupported command '${args.command}'`
+    throw new GantreeError(
+      UNSUPPORTED_OPERATION,
+      `Lib v${frame.config_version} - '${args.command}' is not a valid option for 'command'`
     )
   }
 
@@ -27,7 +34,10 @@ const run = async args => {
   gco = Config.validate(frame, gco)
   gco = Config.preprocess(frame, gco)
 
-  await command(frame, gco)
+  // command(frame, gco)
+  command(frame, gco).catch(e => {
+    handleErr(frame, e)
+  })
 }
 
 module.exports = {
