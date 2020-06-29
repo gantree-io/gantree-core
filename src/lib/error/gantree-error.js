e07fab336a9c754864ae0266e207ecedcd91e8cd
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-06-29 16:01:12 +1000

    fix error throw/handle issues

--- src/lib/error/gantree-error.js
+++ src/lib/error/gantree-error.js
@@ -1,27 +1,43 @@
-const _error_meta = require('./error-meta.json')
-
 const { BaseError } = require('make-error-cause')
 
-const generic_error = _error_meta.errors.generic_error
+const _error_meta = require('./error-meta.json')
+
+const generic_error = _error_meta.errors.GENERIC_ERROR
 
 class GantreeError extends BaseError {
   constructor(
-    code = generic_error.code,
+    meta = generic_error,
     message = generic_error.message,
-    cause = undefined
+    cause = undefined // instance of Error caught
   ) {
     super(message, cause)
 
-    this.code = code
+    this.code = meta.code
+    this.meta = meta
   }
 }
 
 const ErrorTypes = {}
 for (const error_name of Object.keys(_error_meta.errors)) {
-  ErrorTypes[error_name] = error_name
+  ErrorTypes[error_name] = _error_meta.errors[error_name]
+}
+
+const handleErr = (frame, err, throwErr = false) => {
+  const logger = frame.logAt('handleErr')
+  if (typeof err !== GantreeError) {
+    err = new GantreeError(generic_error, err)
+  }
+  logger.error(`FAIL:[${err.meta.code}] ${err.meta.message}: ${err.message}`)
+  if (throwErr == true) {
+    throw err
+  } else {
+    console.log(err)
+    process.exit(err.code)
+  }
 }
 
 module.exports = {
   GantreeError,
-  ErrorTypes
+  ErrorTypes,
+  handleErr
 }
