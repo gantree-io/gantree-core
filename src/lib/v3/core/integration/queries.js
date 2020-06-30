528e56984b41ec889b167332fe6e71c480c429a6
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-06-30 18:08:35 +1000

    add integration queries

---
+++ src/lib/v3/core/integration/queries.js
@@ -0,0 +1,50 @@
+module.exports = {
+  addCliNetwork: `
+    mutation addCliNetwork($cli_nodes: String! $config: String!) {
+        addCliNetwork(cli_nodes: $cli_nodes config: $config)
+    }
+    `,
+  deleteCliNetwork: `
+    mutation deleteCliNetwork($_id: String!) {
+        deleteCliNetwork(_id: $_id)
+    }
+    `,
+  fetchAllNetworks: `
+    query networks {
+        networks {
+            _id
+            project_id
+            chainspec
+            nodes{
+                _id
+                name
+                ip
+                provider
+                status
+                validator
+            }
+        }
+    }
+    `,
+  syncNetwork: `
+    mutation syncNetwork($project_id: String! $platform: String! $network_args: String!) {
+        syncNetwork(project_id: $project_id platform: $platform network_args: $network_args)
+    }
+    `,
+  networkByProjectId: `
+    query networkByProjectId($project_id: String!) {
+        networkByProjectId(project_id: $project_id) {
+            _id
+            project_id
+            chainspec
+            nodes{
+                _id
+                name
+                ip
+                provider
+                status
+                validator
+            }
+        }
+    }`
+}
